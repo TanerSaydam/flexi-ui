96574e6415d584e20b3cba0a57bc4f40488dbe9f
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-10-26 04:21:34 +0300

    flexi-grid pagination bulduğum bir bugu temizledim

--- projects/demo-app/src/app/users/users.component.ts
+++ projects/demo-app/src/app/users/users.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { BlankComponent } from '../blank/blank.component';
 import { FlexiButtonComponent } from '../../../../flexi-button/src/lib/flexi-button.component';
-
-
-
-
+import { FlexiGridModule } from "../../../../flexi-grid/src/lib/modules/flexi-grid.module";
+import { UsersData } from '../../../../documentation/src/app/flexi-grid/data';
 
 @Component({
   selector: 'app-users',
   standalone: true,
   imports: [
     BlankComponent,
-    FlexiButtonComponent
-    ],
+    FlexiButtonComponent,
+    FlexiGridModule
+],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+  data = UsersData
 }
--- projects/documentation/src/app/flexi-grid/data.ts
+++ projects/documentation/src/app/flexi-grid/data.ts
@@ -200,5 +200,315 @@ export const UsersData: UserModel[] = [
         "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/712.jpg",
         "isActive": true,
         "city": "Ankara"
+    },
+    {
+        "id": "559e04f8-5ab6-4e15-a02b-0004b1becb9e",
+        "firstName": "Amanda",
+        "lastName": "Russel",
+        "dateOfBirth": "1984-02-13",
+        "salary": 29903.19,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/650.jpg",
+        "isActive": true,
+        "city": "İstanbul"
+    },
+    {
+        "id": "4b7478e1-8c14-4416-94bf-0006669ec634",
+        "firstName": "Lyle",
+        "lastName": "Kulas",
+        "dateOfBirth": "1961-09-20",
+        "salary": 86158.14,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/801.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "b7d90a7a-0f26-4bf7-90e0-000942c50a4c",
+        "firstName": "Percy",
+        "lastName": "Kutch",
+        "dateOfBirth": "1975-10-04",
+        "salary": 64037.70,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/651.jpg",
+        "isActive": true,
+        "city": "Kayseri"
+    },
+    {
+        "id": "bf9a87fd-6fca-4210-a57b-000a404d0770",
+        "firstName": "Tracy",
+        "lastName": "Howell",
+        "dateOfBirth": "1981-11-21",
+        "salary": 158386.59,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/603.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "5867ac21-4326-4fb0-9329-000d90f3736e",
+        "firstName": "Doug",
+        "lastName": "O'Kon",
+        "dateOfBirth": "1980-09-06",
+        "salary": 50571.94,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/692.jpg",
+        "isActive": false,
+        "city": "Ankara"
+    },
+    {
+        "id": "baaf038b-564e-4641-9b43-000dc979e7b6",
+        "firstName": "Roberto",
+        "lastName": "Morar",
+        "dateOfBirth": "1994-12-30",
+        "salary": 60899.79,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1183.jpg",
+        "isActive": false,
+        "city": "Kayseri"
+    },
+    {
+        "id": "e4390808-1a85-483a-b263-000e36da214f",
+        "firstName": "Travis",
+        "lastName": "Stanton",
+        "dateOfBirth": "1993-12-23",
+        "salary": 248658.15,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/66.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "13d00e5f-37dc-4541-896c-0016d2c8cb6f",
+        "firstName": "Barbara",
+        "lastName": "Treutel",
+        "dateOfBirth": "1958-11-16",
+        "salary": 154438.02,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/500.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "c9d72e76-1e7d-4513-af6c-001a52ec0dad",
+        "firstName": "Amanda",
+        "lastName": "Johnston",
+        "dateOfBirth": "1967-03-07",
+        "salary": 28659.93,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/227.jpg",
+        "isActive": true,
+        "city": "İstanbul"
+    },
+    {
+        "id": "941fd2ba-5ba2-4be6-8280-0020988bcaaf",
+        "firstName": "Franklin",
+        "lastName": "Jones",
+        "dateOfBirth": "1960-09-20",
+        "salary": 178631.03,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/564.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "de638b4b-7b84-42f7-a9ca-0020ce4edf42",
+        "firstName": "Susie",
+        "lastName": "Murray",
+        "dateOfBirth": "1989-10-09",
+        "salary": 241803.13,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1026.jpg",
+        "isActive": false,
+        "city": "Ankara"
+    },
+    {
+        "id": "afe61e10-c277-4716-9fd6-002297b32728",
+        "firstName": "Ismael",
+        "lastName": "Brakus",
+        "dateOfBirth": "1962-01-21",
+        "salary": 27399.36,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1110.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "461ee135-48d7-4b14-b923-002378182c1b",
+        "firstName": "Amelia",
+        "lastName": "Walker",
+        "dateOfBirth": "1973-01-02",
+        "salary": 101128.97,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/745.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "15a27f35-a8a8-4e05-822e-002778da22e6",
+        "firstName": "Bridget",
+        "lastName": "Botsford",
+        "dateOfBirth": "1971-11-25",
+        "salary": 169040.27,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1220.jpg",
+        "isActive": false,
+        "city": "Kayseri"
+    },
+    {
+        "id": "623b3cb7-065e-4bb9-959a-002814f06529",
+        "firstName": "Kerry",
+        "lastName": "Doyle",
+        "dateOfBirth": "1996-07-23",
+        "salary": 92587.80,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1042.jpg",
+        "isActive": true,
+        "city": "İstanbul"
+    },
+    {
+        "id": "23e6616b-ff44-42da-9f79-00288c7bc378",
+        "firstName": "Monique",
+        "lastName": "King",
+        "dateOfBirth": "2002-07-27",
+        "salary": 161433.63,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/798.jpg",
+        "isActive": false,
+        "city": "Ankara"
+    },
+    {
+        "id": "bba89801-e468-43a2-a60a-002bcf4eddeb",
+        "firstName": "Diane",
+        "lastName": "Hagenes",
+        "dateOfBirth": "1965-04-28",
+        "salary": 160262.59,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/380.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "504fc8e1-5962-479c-abf1-0032400c194f",
+        "firstName": "David",
+        "lastName": "Nolan",
+        "dateOfBirth": "1974-02-20",
+        "salary": 21209.33,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/158.jpg",
+        "isActive": true,
+        "city": "Kayseri"
+    },
+    {
+        "id": "0fc8c34c-3ee2-41b6-b251-00328c132042",
+        "firstName": "Aubrey",
+        "lastName": "Jacobs",
+        "dateOfBirth": "1981-08-26",
+        "salary": 244948.91,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/181.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "a6b1a2bc-319b-4bc4-bac9-0035b8194677",
+        "firstName": "Victoria",
+        "lastName": "Kilback",
+        "dateOfBirth": "1960-08-14",
+        "salary": 113962.04,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/712.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "559e04f8-5ab6-4e15-a02b-0004b1becb9e",
+        "firstName": "Amanda",
+        "lastName": "Russel",
+        "dateOfBirth": "1984-02-13",
+        "salary": 29903.19,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/650.jpg",
+        "isActive": true,
+        "city": "İstanbul"
+    },
+    {
+        "id": "4b7478e1-8c14-4416-94bf-0006669ec634",
+        "firstName": "Lyle",
+        "lastName": "Kulas",
+        "dateOfBirth": "1961-09-20",
+        "salary": 86158.14,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/801.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "b7d90a7a-0f26-4bf7-90e0-000942c50a4c",
+        "firstName": "Percy",
+        "lastName": "Kutch",
+        "dateOfBirth": "1975-10-04",
+        "salary": 64037.70,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/651.jpg",
+        "isActive": true,
+        "city": "Kayseri"
+    },
+    {
+        "id": "bf9a87fd-6fca-4210-a57b-000a404d0770",
+        "firstName": "Tracy",
+        "lastName": "Howell",
+        "dateOfBirth": "1981-11-21",
+        "salary": 158386.59,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/603.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "5867ac21-4326-4fb0-9329-000d90f3736e",
+        "firstName": "Doug",
+        "lastName": "O'Kon",
+        "dateOfBirth": "1980-09-06",
+        "salary": 50571.94,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/692.jpg",
+        "isActive": false,
+        "city": "Ankara"
+    },
+    {
+        "id": "baaf038b-564e-4641-9b43-000dc979e7b6",
+        "firstName": "Roberto",
+        "lastName": "Morar",
+        "dateOfBirth": "1994-12-30",
+        "salary": 60899.79,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1183.jpg",
+        "isActive": false,
+        "city": "Kayseri"
+    },
+    {
+        "id": "e4390808-1a85-483a-b263-000e36da214f",
+        "firstName": "Travis",
+        "lastName": "Stanton",
+        "dateOfBirth": "1993-12-23",
+        "salary": 248658.15,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/66.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "13d00e5f-37dc-4541-896c-0016d2c8cb6f",
+        "firstName": "Barbara",
+        "lastName": "Treutel",
+        "dateOfBirth": "1958-11-16",
+        "salary": 154438.02,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/500.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "c9d72e76-1e7d-4513-af6c-001a52ec0dad",
+        "firstName": "Amanda",
+        "lastName": "Johnston",
+        "dateOfBirth": "1967-03-07",
+        "salary": 28659.93,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/227.jpg",
+        "isActive": true,
+        "city": "İstanbul"
+    },
+    {
+        "id": "941fd2ba-5ba2-4be6-8280-0020988bcaaf",
+        "firstName": "Franklin",
+        "lastName": "Jones",
+        "dateOfBirth": "1960-09-20",
+        "salary": 178631.03,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/564.jpg",
+        "isActive": true,
+        "city": "Ankara"
+    },
+    {
+        "id": "a6b1a2bc-319b-4bc4-bac9-0035b8194677",
+        "firstName": "Victoria",
+        "lastName": "Kilback",
+        "dateOfBirth": "1960-08-14",
+        "salary": 113962.04,
+        "avatarUrl": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/712.jpg",
+        "isActive": true,
+        "city": "Ankara"
     }
 ];
