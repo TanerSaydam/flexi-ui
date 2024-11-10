3926eb0488e01f6efdfbd0c2fc36b2104b3d7ab6
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-11-10 06:49:49 +0300

    Btn a özellik ekledim

--- projects/flexi-button/src/lib/flexi-button.component.ts
+++ projects/flexi-button/src/lib/flexi-button.component.ts
@@ -6,7 +6,7 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
   standalone: true,
   imports: [CommonModule],
   template: `
-    <button class="flexi-button flexi-button-{{btnColor}}">
+    <button class="flexi-button flexi-button-{{btnColor}}" [type]="btnType" [disabled]="btnDisabled"]>
     <div class="flexi-button-content" [style]="btnStyle">        
         <span class="material-symbols-outlined flexi-button-size-{{btnSize}}">
           {{ btnIcon }}
@@ -27,8 +27,11 @@ export class FlexiButtonComponent {
   @Input() btnSize: FlexiButtonSizeType = 'default';
   @Input() btnText: string = '';  
   @Input() btnStyle: string = "";
+  @Input() btnType: FlexiButtonType = "button";
+  @Input() btnDisabled: boolean = false;
 }
 
 export type FlexiButtonColorType = "default" | "primary" | "success" | "danger" | "warning" | "info" | "dark";
 
 export type FlexiButtonSizeType = "default" | "small" | "large"
+export type FlexiButtonType = "button" | "submit"
