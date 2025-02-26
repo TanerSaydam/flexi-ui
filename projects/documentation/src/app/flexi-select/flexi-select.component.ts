22c8aa97f35f94986e646c77825f13fe9640ed12
emrahburak <gemrahburak@gmail.com>
2025-02-26 16:09:19 +0300

    UPDATED --i18n/**.json --flexi-select/** flexi-select e ait yerelleştirmeler güncellendi

--- projects/documentation/src/app/flexi-select/flexi-select.component.ts
+++ projects/documentation/src/app/flexi-select/flexi-select.component.ts
@@ -5,6 +5,7 @@ import { CardComponent } from '../blank/card/card.component';
 import { CommonModule } from '@angular/common';
 import SelectFullComponent from './full/full.component'
 import { LoadingComponent } from '../loading/loading.component';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
     selector: 'app-flexi-select',
@@ -15,6 +16,7 @@ import { LoadingComponent } from '../loading/loading.component';
         FlexiSelectModule,
         SelectFullComponent,
         LoadingComponent,        
+        TranslocoModule
     ],
     templateUrl: './flexi-select.component.html',
     styleUrl: './flexi-select.component.css'
--- projects/documentation/src/app/flexi-select/installation/installation.component.ts
+++ projects/documentation/src/app/flexi-select/installation/installation.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { BlankComponent } from '../../blank/blank.component';
 import { CardComponent } from '../../blank/card/card.component';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
     selector: 'app-installation',
     imports: [
         BlankComponent,
         CardComponent,
+        TranslocoModule
     ],
     templateUrl: './installation.component.html',
     styleUrl: './installation.component.css'
